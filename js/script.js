2790bb3e0087667a22a57e5171a6fba5f239149d
ojengdong <http://github.com/ojengdong>
2023-02-16 11:03:49 +0900

    asdd

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const colorsArray = ['red', 'blue', 'pink', 'yellow'];
+console.log(colorsArray);
+// const materialsArray = ['plastic', 'paper'];
+
+// const value = 10;
+
+const colorArray = [
+    ['red', 'blue', 'pink']
+];
+console.log(colorArray[0][1]);
+
+let a = 1;
+let b = a;
+b = 2;
+
+console.log(a);
+console.log(b);
+
+let A = [1, 2, 3];
+let B = A;
+B[0] = 5;
+
+let value1 = 100;
+let value2 = value1;
+
+console.log(value1);
+console.log(value2);
+
+console.log(A);
+console.log(B);
